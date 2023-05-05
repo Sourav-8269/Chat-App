df374e6e892b76b13dd6ba239e94224761c6b3ac
Sourav <souravsahu70047@gmail.com>
2023-05-05 16:05:38 +0530

    Added Register and Login page

---
+++ Frontend/chat-app/src/Components/Register.jsx
@@ -0,0 +1,115 @@
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  HStack,
+  InputRightElement,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import axios from "axios";
+
+export default function Register() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [loading,setLoading]=useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+    axios.post();
+  };
+
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} w={["32%"]} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"} textAlign={"center"}>
+            Register
+          </Heading>
+        </Stack>
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
+          <Stack spacing={4}>
+            <FormControl id="email" isRequired>
+              <FormLabel>Full Name</FormLabel>
+              <Input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </FormControl>
+            <FormControl id="email" isRequired>
+              <FormLabel>Email address</FormLabel>
+              <Input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </FormControl>
+            <FormControl
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              isRequired
+            >
+              <FormLabel>Password</FormLabel>
+              <InputGroup>
+                <Input type={showPassword ? "text" : "password"} />
+                <InputRightElement h={"full"}>
+                  <Button
+                    variant={"ghost"}
+                    onClick={() =>
+                      setShowPassword((showPassword) => !showPassword)
+                    }
+                  >
+                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+            <Stack spacing={10} pt={2}>
+              <Button
+                loadingText="Submitting"
+                size="lg"
+                bg={"blue.400"}
+                color={"white"}
+                _hover={{
+                  bg: "blue.500",
+                }}
+              >
+                Sign up
+              </Button>
+            </Stack>
+            <Stack pt={6}>
+              <Text align={"center"}>
+                Already a user?{" "}
+                <Link to="/login" style={{ color: "#027bff" }}>
+                  Login
+                </Link>
+              </Text>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+}
--- Frontend/chat-app/src/MainRoutes/MainRoutes.jsx
+++ Frontend/chat-app/src/MainRoutes/MainRoutes.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../Components/Home'
 import Chat from '../Components/Chat'
+import Register from '../Components/Register'
+import Login from '../Components/Login'
 
 const MainRoutes = () => {
   return (
     <Routes>
         <Route path='/' element={<Home/>} ></Route>
+        <Route path='/register' element={<Register/>} ></Route>
+        <Route path='/login' element={<Login/>} ></Route>
         <Route path='/chats' element={<Chat/>} ></Route>
     </Routes>
   )
